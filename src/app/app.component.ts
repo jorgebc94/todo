3227e7935977cdde42d412b0bcadaf3fac1a2125
Jorge Cabrera <jorge@Jorges-MBP.attlocal.net>
2021-07-15 14:54:02 -0400

    amplify add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from '../models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todos = [];
+
+  async ngOnInit(){
+    this.getTodos();
+  }
+
+  async createTodo(){
+    await DataStore.save(
+      new Todo({
+      "name": "Lorem ipsum dolor sit amet",
+      "description": "Lorem ipsum dolor sit amet"
+      })
+    );
+  }
+
+  async getTodos(){
+    this.todos = await DataStore.query(Todo);
+  }
+
 }
